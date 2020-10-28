8aec232235442eae535ffe13f276b0efce235bb9
supriyamg1996 <73483413+supriyamg1996@users.noreply.github.com>
2020-10-28 17:04:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 <center>
 <h3>Building a Space Wars game using Python & Pygame</h3>
 <img src="https://github.com/supriyamg1996/pygameshooter/blob/main/Galaxy.png?raw=true" alt="Gspace" width="400" height="400">
-<p>An shooting game that allows to earn points by shooting to obstacle. Built using Python & Pygame Module </p>
+<p>An arcade based shooting game that allows to earn points by shooting to obstacle. Built using Python & Pygame Module </p>
   <a href="https://thecodezine.com/easy-learn-python-space-shooter-game-building-using-pygame/"><b>Complete Source File Here</b></a>
 </center>
